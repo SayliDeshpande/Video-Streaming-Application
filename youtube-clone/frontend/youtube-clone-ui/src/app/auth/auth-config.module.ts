2f6baf932d6e89588185ef3dc7c807c00092394e
sayalitelang <sayalitelang@localhost>
2023-10-01 22:12:40 -0400

    secured frontend angular Application using OAuth2

---
+++ youtube-clone/frontend/youtube-clone-ui/src/app/auth/auth-config.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { AuthModule } from 'angular-auth-oidc-client';
+
+
+@NgModule({
+    imports: [AuthModule.forRoot({
+        config: {
+            authority: 'https://dev-68p18kj7eccfzuba.us.auth0.com',
+            redirectUrl: window.location.origin,
+            clientId: 'b49SBq0tYy6mInKaZvFnMu8p1yMq8KHV',
+            scope: 'openid profile offline_access',
+            responseType: 'code',
+            silentRenew: true,
+            useRefreshToken: true,
+        }
+      })],
+    exports: [AuthModule],
+})
+export class AuthConfigModule {}
